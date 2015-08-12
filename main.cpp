03fcac23fdd5a2eb37a826db147706c45be9d462
Drew Lustro <drewlustro@gmail.com>
2015-08-11 22:26:31 -0700

    Final commit :sunglasses:

--- main.cpp
+++ main.cpp
@@ -1,1 +1,162 @@
-b0b7cb06-40b2-11e5-9e93-b8f6b1194703
+// (c) 2015 Maxrelax
+// @author Drew Lustro
+
+#include <iostream>
+#include <math.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SOIL.h>
+
+#include "shader.h"
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Welcome to Pomona, California!
+";
+
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+
+
+    glfwMakeContextCurrent(window);
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+
+    Shader basicShader = Shader("Shaders/basic.vert", "Shaders/basic.frag");
+
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        // Positions          // Colors           // Texture Coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left
+    };
+    GLuint VBO, VAO, tex0;
+    GLuint EBO;
+
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &tex0);
+    glGenBuffers(1, &EBO);
+
+    int imageWidth, imageHeight;
+    unsigned char* image = SOIL_load_image("Textures/box.jpg", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGB);
+    glBindTexture(GL_TEXTURE_2D, tex0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // first bind to VAO, then bind to vertex buffer(s) and attribute pointers
+    glBindVertexArray(VAO);
+
+        // copy vertices data into VBO
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // copy the index array so we can use an Element Buffer Object (EBO)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+        // set the vertex attrib pointer to walk by 6 floats
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*) 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*) (6 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(2);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind from VBO buffer
+
+    glBindVertexArray(0); // unbind VAO (always a good idea because it prevents incorrect configs / accidental vertex attribs, etc.)
+
+
+
+    while(!glfwWindowShouldClose(window)) {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw
+        basicShader.use();
+        //glUseProgram(shaderProgram);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe mode
+
+        // setup uniforms 'color' and 'time' and update them
+        GLfloat time = glfwGetTime();
+        GLfloat sweetColor = (sin(time) / 2) + 0.5f;
+        GLint uniformTimeLocation = glGetUniformLocation(basicShader.program, "time");
+        GLint uniformSweetColorLocation = glGetUniformLocation(basicShader.program, "sweetColor");
+        glUniform1f(uniformTimeLocation, time);
+        glUniform4f(uniformSweetColorLocation, 0.0f, sweetColor, -sweetColor, 1.0f);
+
+        glBindVertexArray(VAO); // set vertex/attrib config to VAO
+        glBindTexture(GL_TEXTURE_2D, tex0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0); // unbind from VAO
+
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // deallocate
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
